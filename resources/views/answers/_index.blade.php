ff3153f1d1dd7f67bd9de6d888da88d0bbd94b43
javedwasim <javedafaq@gmail.com>
2019-03-04 10:03:38 +0500

    implement functionality of mark best answer of question

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -17,9 +17,21 @@
                             <a title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark this answer as best" class="mt-2">
-                                <i class="fas fa-check fa-2x {{$answer->status}}"></i>
-                            </a>
+                            @can('accept',$answer)
+                                <a title="Mark this answer as best" class="mt-2"
+                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{$answer->id}}').submit();">
+                                    <i class="fas fa-check fa-2x {{$answer->status}}"></i>
+                                </a>
+                                <form id="accept-answer-{{$answer->id}}" method="POST" action="{{route('answers.accept',$answer->id)}}" style="display: none">
+                                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                </form>
+                            @else
+                                @if($answer->is_best)
+                                    <a title="The question owner accepted this answer as best" class="mt-2">
+                                        <i class="fas fa-check fa-2x {{$answer->status}}"></i>
+                                    </a>
+                                @endif
+                            @endcan
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
