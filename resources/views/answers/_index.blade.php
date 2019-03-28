03f9d84935a3b19a6d9859ba125a54744bfad410
javedwasim <javedafaq@gmail.com>
2019-03-28 19:53:58 +0500

    make the vote answer working

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -10,13 +10,24 @@
                 @foreach($answers as $answer)
                     <div class="media">
                         <div class="d-fex flex-column vote-controls">
-                            <a title="This answer is useful" class="vote-up">
+                            <a title="This answer is useful" class="vote-up {{Auth::guest()?'off':''}}"
+                               onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{$answer->id}}').submit();">
                                 <i class="fas fa-caret-up fa-3x"></i>
                             </a>
-                            <span class="votes-count">1230</span>
-                            <a title="This answer is not useful" class="vote-down off">
+                            <form id="up-vote-answer-{{$answer->id}}" method="POST" action="/answers/{{$answer->id}}/vote" style="display: none">
+                                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <input type="hidden" name="vote" value="1">
+                            </form>
+
+                            <span class="votes-count">{{$answer->votes_count}}</span>
+                            <a title="This answer is not useful" class="vote-down off {{Auth::guest()?'off':''}}"
+                               onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{$answer->id}}').submit();">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
+                            <form id="down-vote-answer-{{$answer->id}}" method="POST" action="/answers/{{$answer->id}}/vote" style="display: none">
+                                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <input type="hidden" name="vote" value="-1">
+                            </form>
                             @can('accept',$answer)
                                 <a title="Mark this answer as best" class="mt-2"
                                     onclick="event.preventDefault(); document.getElementById('accept-answer-{{$answer->id}}').submit();">
@@ -39,10 +50,10 @@
                                 <div class="col-4">
                                     <div class="ml-auto">
                                         @can('update',$answer)
-                                            <a href="{{route('questions.answers.edit',[$question->id,$answer->id])}}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            <a href="{{route('questions.answers.edit',[$answer->id,$answer->id])}}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endcan
                                         @can('delete',$answer)
-                                            <form class="form-delete" method="post" action="{{route('questions.answers.destroy',[$question->id,$answer->id])}}">
+                                            <form class="form-delete" method="post" action="{{route('questions.answers.destroy',[$answer->id,$answer->id])}}">
                                                 {{method_field('DELETE')}}
                                                 <input type="hidden" name="_token" value="{{csrf_token()}}">
                                                 <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure?')">Delete</button>
